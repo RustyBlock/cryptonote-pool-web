2308e3117d05bc8317481ee56c9a3a83e0ae583d
cryptoharbour <cryptoharbour@gmail.com>
2018-01-09 06:30:29 +0000

    sumo configs

--- static/config.js
+++ static/config.js
@@ -1,9 +1,9 @@
-var api = "https://etn.rustylock.club:8118";
+var api = "http://sumo.rustylock.club:8117";
 
 var coinUnits = 100;
 var coinDecimalPlaces = 2;
 
-var poolHost = "etn.rustylock.club";
+var poolHost = "sumo.rustylock.club";
 
 var irc = "irc.freenode.net/#rustylock";
 
@@ -11,6 +11,6 @@ var email = "admin@rustylock.club";
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "https://blockexplorer.electroneum.com/block/";
+var blockchainExplorer = "hhttps://explorer.sumokoin.com/block/";
 
-var transactionExplorer = "https://blockexplorer.electroneum.com/tx/";
+var transactionExplorer = "https://explorer.sumokoin.com/tx/";
